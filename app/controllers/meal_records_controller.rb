7133c02ef7349a59bcd9883d6717752262ee60c9
yuuta66jp <yuuta66jp@gmail.com>
2020-03-10 05:27:13 +0000

    [finish]diary編集機能,meal_records編集機能

--- app/controllers/meal_records_controller.rb
+++ app/controllers/meal_records_controller.rb
@@ -19,6 +19,15 @@ class MealRecordsController < ApplicationController
   end
 
   def edit
+    @meal_record =MealRecord.find(params[:id])
+  end
+
+  def update
+    @meal_record =MealRecord.find(params[:id])
+    @meal_record.update(meal_record_params)
+
+    redirect_to edit_diary_path(@meal_record.diary_id)
+  end
 
   private
   def meal_record_params
