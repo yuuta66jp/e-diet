3361458f2f1bf40e5b11d8f3757d3bf14f046366
yuuta66jp <yuuta66jp@gmail.com>
2020-03-10 10:01:22 +0000

    [finish]タグ機能

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -62,7 +62,7 @@ class DiariesController < ApplicationController
 
   private
   def diary_params
-    params.require(:diary).permit(:user_id, :remark, :activity_status)
+    params.require(:diary).permit(:user_id, :remark, :activity_status, :tag_list)
   end
 
 end
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -8,4 +8,8 @@ class Diary < ApplicationRecord
   # enum機能の定義
   enum activity_status: { 低い: 0, 普通: 1, 高い: 2 }
 
+  # タグ機能
+  # acts_as_taggable_on :tagsと同じ意味（エイリアス）
+  acts_as_taggable
+
   end
