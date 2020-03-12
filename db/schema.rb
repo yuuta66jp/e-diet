bc8d5717c9e2c49626aa98b52a4c59ea7ec2a067
yuuta66jp <yuuta66jp@gmail.com>
2020-03-12 00:41:45 +0000

    [start]Admin作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_024557) do
+ActiveRecord::Schema.define(version: 2020_03_12_004001) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "body_weights", force: :cascade do |t|
     t.integer "user_id", null: false
