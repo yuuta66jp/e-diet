30a3ca2606d740dfc284ddfdc4cce612ce8aa14b
yuuta66jp <yuuta66jp@gmail.com>
2020-03-12 01:40:45 +0000

    [start]admin側のuser,diary設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'diaries#index'
 
+  namespace :admins do
+    resources :users
+    resources :diaries
+  end
+
   resources :users,        except: [:new, :create]
   get       'congfirm' => 'users#congfirm'
 
