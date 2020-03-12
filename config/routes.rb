897ef04b6d786a3bed7c4f56b1381c6a19b556be
yuuta66jp <yuuta66jp@gmail.com>
2020-03-12 14:19:06 +0000

    [add]ポイント機能(sign up時)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users
+  # deviseコントローラーのカスタマイズ(新規登録時のpoint作成のため)
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'diaries#index'
 
