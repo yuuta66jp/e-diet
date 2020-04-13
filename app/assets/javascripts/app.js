29cba292c0efb61614ff4082d911a8ea863bd22b
yuuta66jp <yuuta66jp@gmail.com>
2020-04-13 12:57:44 +0000

    [add]jQuery適正体重表示機能

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("#height").change(function() {
+    $('.green').remove();
+    var height = $("#height").val();
+    var result = (height / 100) * (height / 100) * 22;
+    var weight = Math.round(result);
+    $("#text").after("<p class='green'>あなたの適正体重は" + weight + "kgです！<br>無理の無いダイエットを心がけよう！</p>");
+  });
+});
