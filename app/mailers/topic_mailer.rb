4d850517a3f9d7ddee07e5434517f3ce1385883d
yuuta66jp <yuuta66jp@gmail.com>
2020-05-03 11:02:12 +0000

    [fix]メール配信機能

--- app/mailers/topic_mailer.rb
+++ app/mailers/topic_mailer.rb
@@ -1,7 +1,7 @@
 class TopicMailer < ApplicationMailer
   # 一斉配信設定
   default to: -> { User.pluck(:email) },
-          from: "yuuta66jo@gmail.com"
+          from: "yuuta66jp@gmail.com"
 
   def topic
     mail(subject: '新規トピックス配信のお知らせ')
